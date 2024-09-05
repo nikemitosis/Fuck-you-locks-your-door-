111acfc72ebf541a472029b863ee71251d2942be
nikemitosis <nickmitsos20@gmail.com>
2024-09-05 00:24:59 -0400

    Initial Commit
    Learned why C's are sharp and why Bep was In his Ex

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace FuckYouLocksYourDoors;
+
+[BepInPlugin(Plugin.GUID, Plugin.NAME, Plugin.VERSION)]
+public class Plugin : BaseUnityPlugin {
+	const string GUID = "nikemitosis.lethal_company.fylyd";
+	const string NAME = "Fuck you, *locks your doors*";
+	const string VERSION = "1.0.0";
+	
+	internal static new ManualLogSource Logger;
+	
+	private readonly Harmony harmony = new Harmony(GUID);
+	
+	private void Awake() {
+		Logger = base.Logger;
+		harmony.PatchAll();
+		
+		Logger.LogInfo($"Retrieved the keys to lock your doors");
+	}
+}
+
+[HarmonyPatch(typeof(DoorLock))]
+class DoorPatch {
+
+	[HarmonyPatch("Awake")]
+	[HarmonyPostfix]
+	static void AutoLock(DoorLock __instance) {
+		__instance.LockDoor();
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This is an extremely simple (literally one patch with one line) Lethal Company mod that forces all doors to start as locked. Because fuck you. 
+
+This mod is (currently) not configurable, but it may be in the future. 
